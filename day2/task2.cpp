c29d44fd6de04fcac324907a4eb9bfcc40c50ac9
jaked0626 <jakez0626@gmail.com>
2023-12-05 03:09:40 +0900

    DONE: finally freed of parsing strings in this crazy outlaw language

--- day2/task2.cpp
+++ day2/task2.cpp
@@ -1,18 +1,83 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 
-int main() {
+std::vector<std::string> splitString(const std::string &inputString, char delimiter) {
+    std::vector<std::string> tokens;
+    std::size_t start {0};
+    std::size_t end { inputString.find(delimiter, start) };
+
+    while (end != std::string::npos) 
+    {
+        // avoiding empty strings
+        if (end != start) 
+        {
+          tokens.push_back(inputString.substr(start, end - start));
+        }
+        start = end + 1;
+        end = inputString.find(delimiter, start);
+    }
+
+    // push last token 
+    if (end != start) 
+    {
+       tokens.push_back(inputString.substr(start));
+    }
+    return tokens;
+}
+
+int powerOfGame(std::string line) 
+{
+    using std::vector;
+    using std::string; 
+    using std::stoi;
+    
+    std::unordered_map<string, int> maxBalls {};
+    int gameScore {1};
+
+    vector<string> gameAndContent { splitString(line, ':') };
+    string content { gameAndContent[1] };
+    vector<string> gameResults { splitString(content, ' ') };
+
+    for (size_t i = 0; i < gameResults.size(); i += 2)
+    {   
+        int num { stoi(gameResults[i]) };
+        string color { gameResults[i + 1] };
+
+        // process trailing , or ;
+        color.erase(color.find_last_not_of(" ,;") + 1);
+
+        if (maxBalls.find(color) == maxBalls.end()) 
+        {
+            maxBalls[color] = 0;
+        }
+        maxBalls[color] = std::max(maxBalls[color], num);
+    }
+
+    for (const auto& maxNum : maxBalls) 
+    {
+        gameScore *= maxNum.second;
+    }
+
+    return gameScore;
+}
+
+
+int main() 
+{
     std::string filePath {"./day2/input.txt"};
     std::ifstream file(filePath);
+
+    int sum {};
 
     if (file.is_open()) 
     {
         std::string line {};
         while (std::getline(file, line))
         {
-            std::cout << line << "\n";
+            sum += powerOfGame(line);
         }
 
         file.close();
@@ -21,6 +86,8 @@ int main() {
     {
         std::cerr << "Error: Unable to open file " << filePath << "\n";
     }
+
+    std::cout << sum << "\n";
 
     return 0;
 }
